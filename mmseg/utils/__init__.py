a070e3431be1a25a4d7dd4e089c47271cb4968a5
Fajar Luhung Parasdyo <fajarluhung@gmail.com>
2022-09-25 19:53:57 +0700

    add predict utils

--- mmseg/utils/__init__.py
+++ mmseg/utils/__init__.py
@@ -5,6 +5,7 @@ from .misc import find_latest_checkpoint
 from .set_env import setup_multi_processes
 from .util_distribution import build_ddp, build_dp, get_device
 from .convert_and_split_dataset import *
+from .predict import *
 
 __all__ = [
     'get_root_logger', 'collect_env', 'find_latest_checkpoint',
---
+++ mmseg/utils/predict.py
@@ -0,0 +1,49 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from ast import arg
+from genericpath import isfile
+import os
+import glob
+
+from mmseg.apis import inference_segmentor, init_segmentor, show_result_pyplot
+from mmseg.core.evaluation import get_palette
+
+
+def ext(path):
+    return os.path.splitext(path)[1]
+
+
+def predict(config, checkpoint, img, out_file=None, device='cuda:0', palette='rust', opacity=0.5):
+    # make output folder
+    if not out_file:
+        out_dir = 'results'
+    elif os.path.splitext(out_file)[1] == '':
+        out_dir = out_file
+    else:
+        raise Exception('Sorry, output must be a folder')
+    os.makedirs(out_dir, exist_ok=True)
+
+    # check if the input from a single image file or from a folder
+    img_exts = ['.jpg', '.JPG', '.jpeg', '.png']
+    if  os.path.isdir(img):
+        imgs_path = [img_path for img_path in glob.glob(f'{img}/**/*.*', recursive=True) if ext(img_path) in img_exts]
+    elif os.path.isfile(img):
+        imgs_path = [img]
+    else:
+        raise Exception('Sorry, input must be an image file or a folder')
+
+    # get palette array
+    palette_array = get_palette(palette) if isinstance(palette, str) else palette
+    # build the model from a config file and a checkpoint file
+    model = init_segmentor(config, checkpoint, device=device)
+    # predict
+    for img_path in imgs_path:
+        img_file = os.path.split(img_path)[1]
+        result = inference_segmentor(model, img_path)
+        # show the results
+        show_result_pyplot(
+            model,
+            img_path,
+            result,
+            palette_array,
+            opacity=opacity,
+            out_file=os.path.join(out_dir, img_file))
